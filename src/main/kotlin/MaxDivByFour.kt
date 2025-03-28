a08ab17a73201e4c75419a09d2ddd7b18a4f7005
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2025-03-28 08:10:37 -0500

    Exercise on filtered numbers.

--- src/main/kotlin/MaxDivByFour.kt
+++ src/main/kotlin/MaxDivByFour.kt
@@ -1,2 +1,12 @@
-class MaxDivByFour {
+fun main() {
+    val n = readln().toInt() // Read number of elements to enter
+    val numbers = mutableListOf<Int>()
+    repeat(n) {
+        numbers.add(readln().toInt()) // Read each number and add it to the list
+    }
+    // Find the maximum number from the filtered list
+    val filteredNumbers = numbers.filter { it % 4 == 0 }
+
+    println(filteredNumbers.maxOrNull()) // Print the maximum number divisible by 4
+
 }
