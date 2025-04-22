95053e4261a1d6d651592b46dc6fbabc19f84482
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2025-04-22 18:32:00 -0500

    Strings topic exercises.

---
+++ src/main/kotlin/strings/ComparingStrings.kt
@@ -0,0 +1,12 @@
+package strings
+
+fun main() {
+    val first = "first"
+    val second = "second"
+    var str = "first"
+
+    println(first == str)    // true
+    println(first == second) // false
+    println(first != second) // true
+    println(second == str) // false
+}
---
+++ src/main/kotlin/strings/Immutability.kt
@@ -0,0 +1,21 @@
+package strings
+
+fun main() {
+    // Immutable strings
+    val str = "Hello, Kotlin!"
+    println("Original string: $str") // Hello, Kotlin!
+
+    // Attempting to modify the string (this will not change the original string)
+    val modifiedStr = str.replace("Kotlin", "Clarice")
+    println("Modified string: $modifiedStr") // Hello, Clarice!
+    println("Original string after modification attempt: $str") // Hello, Kotlin!
+
+    // Strings are immutable in Kotlin
+    // str[0] = 'h' // This will cause a compilation error
+
+    // If you want to change the string, you can reassign it
+    var mutableStr = "String"
+    println("Mutable string: $mutableStr") // String
+    mutableStr = "Mutable"
+    println("Reassigned string: $mutableStr") // Mutable
+}
