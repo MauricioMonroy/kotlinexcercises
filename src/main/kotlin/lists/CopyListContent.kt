1aa3a0984725fecc8fe36888e5f77a6fb25162fc
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2025-04-26 19:00:50 -0500

    Lists.

---
+++ src/main/kotlin/lists/CopyListContent.kt
@@ -0,0 +1,13 @@
+package lists
+
+fun main() {
+    // Copying a list
+    val list = mutableListOf("A", "B", "C")
+    val copy = list.toMutableList() // This function creates a new mutable list with the same elements as the original list
+    print(copy) // [A, B, C]
+
+    // Copying a list with a range
+    val list2 = mutableListOf("A", "B", "C", "D", "E")
+    val copy2 = list2.subList(1, 4) // This function creates a new mutable list with the elements from index 1 to index 4 (exclusive)
+    print(copy2) // [B, C, D]
+}
