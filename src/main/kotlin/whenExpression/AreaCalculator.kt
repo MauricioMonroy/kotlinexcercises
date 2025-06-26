f61976de363e1af914909b66fefb5a3c64637012
MauricioMonroy <mauriciomonroy@live.cl>
2025-06-25 20:16:07 -0500

    The fun never ends.

---
+++ src/main/kotlin/whenExpression/AreaCalculator.kt
@@ -0,0 +1,37 @@
+package whenExpression
+
+import kotlin.math.sqrt
+
+fun triangle(a: Double, b: Double, c: Double): Double {
+    val s = (a + b + c) / 2
+    return sqrt(s * (s - a) * (s - b) * (s - c))
+}
+
+fun rectangle(a: Double, b: Double): Double = a * b
+fun circle(radius: Double): Double = 3.14 * radius * radius
+
+fun main() {
+    val shape = readln()
+    val area = when (shape) {
+        "triangle" -> {
+            val a = readln().toDouble()
+            val b = readln().toDouble()
+            val c = readln().toDouble()
+            triangle(a, b, c)
+        }
+
+        "rectangle" -> {
+            val a = readln().toDouble()
+            val b = readln().toDouble()
+            rectangle(a, b)
+        }
+
+        "circle" -> {
+            val r = readln().toDouble()
+            circle(r)
+        }
+
+        else -> throw IllegalArgumentException("Unknown shape: $shape")
+    }
+    println(area)
+}
---
+++ src/main/kotlin/whenExpression/TheArmyOfUnits.kt
@@ -0,0 +1,19 @@
+package whenExpression
+
+fun main() {
+    val units = readln().toInt()
+    println(
+        when (units) {
+            in Int.MIN_VALUE until 1 -> "no army"
+            in 1..4 -> "few"
+            in 5..9 -> "several"
+            in 10..19 -> "pack"
+            in 20..49 -> "lots"
+            in 50..99 -> "horde"
+            in 100..249 -> "throng"
+            in 250..499 -> "swarm"
+            in 500..999 -> "zounds"
+            else -> "legion"
+        }
+    )
+}
