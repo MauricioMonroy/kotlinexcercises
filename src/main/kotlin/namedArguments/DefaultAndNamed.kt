b5bdf7815e61330648f808dd21cf213794d5b045
Mauricio Alberto Monroy Calle <mauriciomonroy@live.cl>
2025-07-08 15:37:49 -0500

    Update DefaultAndNamed.kt

--- src/main/kotlin/namedArguments/DefaultAndNamed.kt
+++ src/main/kotlin/namedArguments/DefaultAndNamed.kt
@@ -1,1 +1,11 @@
+fun calcEndDayAmount(startAmount: Int = 0, ticketPrice: Int, soldTickets: Int) =
+        startAmount + ticketPrice * soldTickets
 
+fun main() {
+    // val amount = calcEndDayAmount(10, 500)  // Won't work —
+                                        // no value for soldTickets
+    val amount = calcEndDayAmount(
+        ticketPrice = 10, 
+        soldTickets = 500)  
+    println(amount) // 5000
+}
