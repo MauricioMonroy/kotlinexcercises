8f92b4fa2f085bcc2a75200a9ab54ce60395584a
Mauricio Alberto Monroy Calle <mauriciomonroy@live.cl>
2025-07-08 15:45:25 -0500

    Update namedAndDefault.kt

--- src/main/kotlin/namedArguments/namedAndDefault.kt
+++ src/main/kotlin/namedArguments/namedAndDefault.kt
@@ -1,1 +1,11 @@
+// The default values for function parameters may or may not be constant. 
+// They can also be another named argument or a function.
+fun sum2(a: Int, b: Int = a) = a + b
 
+fun main() {
+    val calculate = sum2(1) // 1 + 1
+	val calculate2 = sum2(2, 3) // 2 + 3	
+    
+    println("The first sum is equal to: $calculate")
+    println("The second sum is equal to: $calculate2")
+}
