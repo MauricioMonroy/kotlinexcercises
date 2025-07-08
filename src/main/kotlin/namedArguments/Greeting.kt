552c98e6cb740c202214b9e3d69292527f531678
Mauricio Alberto Monroy Calle <mauriciomonroy@live.cl>
2025-07-08 16:09:23 -0500

    Update Greeting.kt

--- src/main/kotlin/namedArguments/Greeting.kt
+++ src/main/kotlin/namedArguments/Greeting.kt
@@ -1,1 +1,27 @@
+// The greetNeo() function returns "Hello, Mr. Anderson" with correct arguments
+// Otherwise returns "MAtrix error"
+fun greetUser(
+    name: String,
+    admin: Boolean = false,
+    smith: Boolean = false,
+    honorific: String = "",
+    greet: String = "Greetings"
+): String {
+    return if (!admin && !smith) {
+        "$greet, $honorific $name"
+    } else {
+        "Matrix Error"
+    }
+}       
 
+fun greetNeo() = greetUser(
+    "Anderson",
+    false,
+    false,
+    "Mr.",
+    "Hello",
+)
+
+fun main() {
+    println(greetNeo())
+}
