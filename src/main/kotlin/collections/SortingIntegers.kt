458e3ba921c6d98ba9453943b6ab8fb435a4c2e2
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2025-07-21 14:06:05 -0500

    Introduction to collections.

---
+++ src/main/kotlin/collections/SortingIntegers.kt
@@ -0,0 +1,11 @@
+package collections
+
+fun sortIntegers(integers: List<Int>): List<Int> {
+    return integers.sorted()
+}
+
+fun main() {
+    val input = readln().split(" ").map { it.toInt() }
+    val sortedIntegers = sortIntegers(input)
+    println(sortedIntegers.joinToString(" "))
+}
