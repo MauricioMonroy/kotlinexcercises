3d0cf5e0b0695842e849f25c4c603b98665b96a0
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2025-07-30 10:25:09 -0500

    New "members".

---
+++ src/main/kotlin/collections/list/ProcessGroceries.kt
@@ -0,0 +1,15 @@
+package collections.list
+
+fun processGroceries(groceries: MutableList<String>) {
+    groceries.reverse()
+    for ((index, item) in groceries.withIndex()) {
+        if (!item.startsWith('A')) {
+            println("$index: $item")
+        }
+    }
+}
+
+fun main() {
+    val groceries = mutableListOf("Apple", "Banana", "Avocado", "Orange", "Grapes")
+    processGroceries(groceries)
+}
---
+++ src/main/kotlin/collections/list/RepeatedInteger.kt
@@ -0,0 +1,14 @@
+package collections.list
+
+fun main() {
+    val numbers = readln().split(" ").map { it.toInt() }
+    val occurrences = mutableMapOf<Int, Int>()
+    for (number in numbers) {
+        if (occurrences.containsKey(number)) {
+            println(number)
+            return
+        }
+        occurrences[number] = 1
+    }
+    println(-1)
+}
