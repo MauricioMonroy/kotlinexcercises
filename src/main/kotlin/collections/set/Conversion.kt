6fd7dcb625d9fd6b95e531100f59ae46888cb760
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2025-08-13 09:26:24 -0500

    Topic 'set' examples and exercises.

---
+++ src/main/kotlin/collections/set/Conversion.kt
@@ -0,0 +1,11 @@
+package collections.set
+
+fun conversion(strings: MutableList<String>): Set<String> {
+    return strings.toSet()
+}
+
+fun main() {
+    val inputStrings = readln().split(" ")
+    val resultSet = conversion(inputStrings.toMutableList())
+    println(resultSet.joinToString(" "))
+}
---
+++ src/main/kotlin/collections/set/Initialization.kt
@@ -0,0 +1,24 @@
+package collections.set
+
+val humanLanguages = setOf<String>("English", "Spanish", "French") // immutable set of human languages
+val languages = setOf("Java", "Kotlin", "Python") // immutable set of programming languages
+val numbers = emptySet<Int>() // empty immutable set of integers
+val letters = setOf<Char>('A', 'B', 'C') // immutable set of characters
+val set = buildSet<Char> {
+    add('D')
+    addAll(letters)
+    add('E')
+} // buildSet creates a mutable set and returns it as an immutable set
+
+fun main() {
+    println(humanLanguages) // Output: [English, Spanish, French]
+    println(languages) // Output: [Java, Kotlin, Python]
+    println(numbers) // Output: []
+    println(letters) // Output: [A, B, C]
+    println(set) // Output: [D, A, B, C, E]
+    // Note: The order of elements in a set is not guaranteed, so the output may
+    // vary in terms of the order of elements.
+    // However, the elements themselves will always be the same.
+    // For example, the output could also be: [A, B, C, D, E] or [E, D, A, B, C] etc.
+    // The important thing is that all elements are present and no duplicates exist.
+}
