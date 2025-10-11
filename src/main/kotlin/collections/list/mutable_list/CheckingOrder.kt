5bed8b23bb1c9eb6df3807eb58680022af3f60bb
MauricioMonroy <mauriciomonroy@live.cl>
2025-10-11 16:34:29 -0500

    Problem of the day: Lists

---
+++ src/main/kotlin/collections/list/mutable_list/CheckingOrder.kt
@@ -0,0 +1,8 @@
+package collections.list.mutable_list
+
+fun main() {
+    val numbers = mutableListOf<Int>()
+    repeat(readln().toInt()) { numbers.add(readln().toInt()) }
+    val isAscending = numbers.zipWithNext().all { (a, b) -> a <= b }
+    println(if (isAscending) "YES" else "NO")
+}
