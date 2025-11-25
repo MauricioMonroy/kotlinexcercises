0b3d34b68554c7e396ca4a688d1b80f8248c9d1d
Mauricio Monroy <mauriciomonroy@live.cl>
2025-11-25 15:40:32 -0500

    Topic: Multidimensional Arrays.

---
+++ src/main/kotlin/types_and_data_structures/arrays/multi_dimensional_arrays/TwoXThreeMatrix.kt
@@ -0,0 +1,9 @@
+package types_and_data_structures.arrays.multi_dimensional_arrays
+
+fun main() {
+    val accordance = arrayOf(
+        arrayOf("[0][0]", "[0][1]", "[0][2]"),
+        arrayOf("[1][0]", "[1][1]", "[1][2]")
+    )
+    println(accordance.contentDeepToString())
+}
